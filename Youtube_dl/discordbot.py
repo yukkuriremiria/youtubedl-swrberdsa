57dffa94ed8e6b9081b68b44bf6798ead056d177
yukkuriremiria <98326743+yukkuriremiria@users.noreply.github.com>
2022-07-08 21:32:16 +0900

    Update discordbot.py

--- Youtube_dl/discordbot.py
+++ Youtube_dl/discordbot.py
@@ -1,21 +1,47 @@
-from discord.ext import commands
-from os import getenv
-import traceback
-
-bot = commands.Bot(command_prefix='/')
+from flask import Flask, render_template, request, send_file
+from subprocess import call
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+# init_application
+app = Flask(__name__)
 
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 
-token = getenv('DISCORD_BOT_TOKEN')
-bot.run(token)
+@app.route('/download_video')
+def download_mp4():
+    url = request.args.get('url')
+    filename = 'test'
+
+    cmd = 'youtube-dl -o export_tmp/videos/' + \
+        filename + ' --no-cache-dir ' + url
+    call(cmd.split(' '), shell=False)
+
+    try:
+        return send_file(
+            'export_tmp\\videos\\test.mp4',
+            as_attachment=True,
+            attachment_filename='test.mp4',
+            mimetype='video/mp4'
+        )
+    except:
+        return send_file(
+            'export_tmp\\videos\\test.mkv',
+            as_attachment=True,
+            attachment_filename='test.mp4',
+            mimetype='video/mp4'
+        )
+    else:
+        return send_file(
+            'export_tmp\\videos\\test.webm',
+            as_attachment=True,
+            attachment_filename='test.mp4',
+            mimetype='video/mp4'
+        )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
